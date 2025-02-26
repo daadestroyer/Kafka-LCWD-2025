792fd1a0403eb64240c7fc35245d1e3152895921
daadestroyer <nigamshubham2000@gmail.com>
2025-02-26 23:54:36 +0530

    EndUser and DeliveryBoy 2 project created

---
+++ KafkaEndUser/src/test/java/com/thecoderstv/KafkaEndUser/KafkaEndUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecoderstv.KafkaEndUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEndUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
